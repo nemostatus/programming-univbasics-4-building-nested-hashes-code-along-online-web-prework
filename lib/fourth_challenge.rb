dfd9a42b1a1318b6e18d9240a9b4792261daed45
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-11-29 10:48:31 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,7 +15,7 @@ def fourth_challenge
       :heroine_friends => [
         {name: "Steven",age: "30",attitude: "confused"},
         {name: "Nurse",age:"44",attitude:"worried"}
-      ]}
+      ]
    }
 }
 
