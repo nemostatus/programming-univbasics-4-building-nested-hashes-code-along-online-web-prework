deb06390480f3f5c1d7c4b4ead4ed02f31e0ce6b
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-11-29 11:16:13 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,11 +22,10 @@ def third_challenge
         },
       :heroine => {
         name: "Juliet", age:"15",
-        status: "alive"
-        },
+        status: "alive"},
       :heroine_friends => []
    }
   }
 
-end
+
 end
